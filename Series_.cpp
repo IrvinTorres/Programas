ac05893ca2e3dab9e6dfcc263b1fd4ecbce4dc3f
IrvinJassielHernandezTorres <71141131+IrvinTorres@users.noreply.github.com>
2020-10-12 23:18:23 -0500

    Add files via upload

---
+++ Series_.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+ int main()
+
+ {
+     int conteo;
+     conteo=100;
+
+     while (conteo>=0)
+     {
+    printf("%i\n",conteo);
+     conteo=conteo-1;
+
+    }
+     return 0;
+ }
